6098084755dc48760d9371f69bd6a0b8dc7e6e1f
Aditi Londhe <lavendereverything7@gmail.com>
2025-02-23 22:46:53 +0530

    update card

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,40 +1,109 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useContext, useState } from "react";
 import { FaHeart } from "react-icons/fa";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { AuthContext } from "../contexts/AuthProvider";
+import Swal from "sweetalert2";
 const Cards = ({ item }) => {
-  const [isHeartfilled, setIsheartfilled] = useState(false);
+  const { name, image, price, recipe, _id } = item;
+  // console.log(item)
+  const [isHeartFilled, setIsHeartFilled] = useState(false);
+  const { user } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+  const location = useLocation();
   const handleHeartClick = () => {
-    setIsheartfilled(!isHeartfilled);
+    setIsHeartFilled(!isHeartFilled);
+  };
+
+  // add to cart
+  const handleAddtoCart = (item) => {
+    //console.log("btn is clicked", item);
+
+    if (user && user?.email) {
+      const cartItem = {
+        menuItemId: _id,
+        name,
+        quantity: 1,
+        image,
+        price,
+        recipe,
+        email: user.email,
+      };
+      console.log(cartItem);
+
+      fetch("http://localhost:6001/carts", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(cartItem),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          //console.log(data);
+          if (data.insertedId) {
+            Swal.fire({
+              position: "top-end",
+              icon: "success",
+              title: "Your work has been saved",
+              showConfirmButton: false,
+              timer: 1500,
+            });
+          }
+        });
+    } else {
+      Swal.fire({
+        title: "Signup or Login",
+        text: "Please create an account or log in to continue ",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Sign Up Now",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          navigate("/signup", { state: { from: location } });
+        }
+      });
+    }
   };
   return (
-    <div className="card bg-base-100 w-96 shadow-xl relative bg-white">
+    <div
+      to={`/menu/${item._id}`}
+      className="card shadow-xl relative mr-5 md:my-5"
+    >
       <div
         className={`rating gap-1 absolute right-2 top-2 p-4 heartStar bg-green ${
-          isHeartfilled ? "text-rose-500" : "text-white"
+          isHeartFilled ? "text-rose-500" : "text-white"
         }`}
         onClick={handleHeartClick}
       >
-        <FaHeart className="h-5 w-5 cursor-pointer" />
+        <FaHeart className="w-5 h-5 cursor-pointer" />
       </div>
       <Link to={`/menu/${item._id}`}>
         <figure>
           <img
             src={item.image}
             alt="Shoes"
-            className="hover:scale-105 transition-all duration-200 md:h-72"
+            className="hover:scale-105 transition-all duration-300 md:h-72"
           />
         </figure>
       </Link>
       <div className="card-body">
-        <h2 className="card-title">{item.name}</h2>
+        <Link to={`/menu/${item._id}`}>
+          <h2 className="card-title">{item.name}!</h2>
+        </Link>
         <p>Description of the item</p>
         <div className="card-actions justify-between items-center mt-2">
           <h5 className="font-semibold">
-            {" "}
-            <span className="text-sm text-red">$</span>
-            {item.price}
+            <span className="text-sm text-red">$ </span> {item.price}
           </h5>
-          <button className="btn bg-green text-white">Buy Now</button>
+          <button
+            className="btn bg-green text-white "
+            onClick={() => handleAddtoCart(item)}
+          >
+            Add to Cart{" "}
+          </button>
         </div>
       </div>
     </div>
